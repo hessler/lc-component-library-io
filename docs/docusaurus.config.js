c867b044ead844742cabb15ca440ec7619dca298
hessler <anthonyhessler@gmail.com>
2022-12-15 16:55:11 -0500

    Update Base URL in Docusaurus config.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const title = 'Life.Church Web Services Component Library';
 
 const config = {
-  baseUrl: '/',
+  baseUrl: '/lc-component-library-io/',
   deploymentBranch: 'gh-pages', // The name of the GitHub deployment branch.
   favicon: 'https://www.life.church/favicon-32x32.png',
   i18n: {
