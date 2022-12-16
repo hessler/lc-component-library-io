8d10140f733c786ca731d93426d2b67014639630
hessler <anthonyhessler@gmail.com>
2022-12-16 11:05:42 -0500

    Additional entry point update and export adjustment.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,4 @@
 /* eslint-disable jsdoc/require-file-overview */
-import * as components from './components';
-import * as themes from './themes';
-import * as utils from './utils';
-
-export { components, themes, utils };
+export * from './components';
+export * from './themes';
+export * from './utils';
